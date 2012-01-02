a0f667cf05ab9986cbf5ac19b6b746d15512736e
Ken Zheng <kzheng@line2.com>
2012-01-02 15:32:58 -0800

    Added home, teamstats, and login pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
+  end
+  
+  def teamstats
+    @title = "Team Stats"
+  end
+  
+  def login
+    @title = "Login"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Wallboard::Application.routes.draw do
   get "pages/home"
+  get "pages/teamstats"
+  get "pages/login"
 
   resources :users
 
@@ -52,7 +54,7 @@ Wallboard::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
